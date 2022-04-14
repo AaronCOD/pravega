ed186caed168f5e054cf44d6b45fb6000a80bafc
Aaron Wu <Aaron.Wu@dell.com>
2022-04-14 17:07:41 +0800

    add logs

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/WriteOperation.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/WriteOperation.java
@@ -94,6 +94,7 @@ class WriteOperation implements Callable<CompletableFuture<Void>> {
         traceId = LoggerHelpers.traceEnter(log, "write", handle, offset, length);
         timer = new Timer();
         this.hackDelayMillis = hackDelayMillis;
+        log.info("WriteOperation delay {}", hackDelayMillis);
     }
 
     @Override
